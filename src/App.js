595b8ce3f1c660dfb8a7cdbeaa04d71edbac262b
DavidKielty1 <davidkielty@live.co.uk>
2022-02-14 15:49:36 +0000

    Added cardCtx ContextProvider wrapper, headerCartButton badge context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals.js";
 import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const closeCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onCloseCart={closeCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -4,12 +4,12 @@ import mealsImage from "../../assets/food.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton.js";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h2>ReactMeals</h2>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious meals" />
