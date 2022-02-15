33730b0328ff9bb931468c3b4067755bcc95bd92
DavidKielty1 <davidkielty@live.co.uk>
2022-02-15 17:38:41 +0000

    useRef with useContext added to cart input

--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import MealItemForm from "./MealItemForm";
-
 import classes from "./MealItem.module.css";
+import CartContext from "../../store/cart-context";
 
 export default function MealItem(props) {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -14,7 +24,7 @@ export default function MealItem(props) {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <MealItemForm  id={props.id}/>
+      <MealItemForm onAddToCart={addToCartHandler} id={props.id} />
     </li>
   );
 }
